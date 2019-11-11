7ab14562dec1805721010339ee32dccf67028b14
kodomazer <brianzha@gmail.com>
2019-11-10 23:24:29 -0800

    Table Logic
    Implement the data logic for Table

--- Assets/Scripts/Table.cs
+++ Assets/Scripts/Table.cs
@@ -15,25 +15,25 @@ public class Table : MonoBehaviour
         }
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void feedDish(FoodType food)
     {
-
+        for(int i = 0; i < dishCount; i++)
+        {
+            if(!fed[i] && RequestedFood[i] == food)
+            {
+                fed[i] = true;
+                //Update Visuals
+                return;
+            }
+        }
+        //Do we care if they're fed something they don't want to eat?
     }
 
     public void setFoodRequest(FoodType[] foods)
     {
+        RequestedFood = foods;
+        fed = new bool[dishCount];
 
+        //Update Visuals
     }
 }
