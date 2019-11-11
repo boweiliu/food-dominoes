ce860c227e15d6b9e1f747c0d00547b8976127ed
Alice Zhao <alice.zhao@citcon-inc.com>
2019-11-10 21:36:49 -0800

    Snapping to grid

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class GridManager : MonoBehaviour
 {
@@ -8,7 +9,7 @@ public class GridManager : MonoBehaviour
     public float offsetX;
     public int columns;
     public float offsetY;
-    public float tileScale;
+    public float delta;
 
     // Start is called before the first frame update
     void Start()
@@ -20,13 +21,17 @@ public class GridManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        gameObject.transform.position = SnapToGridPosition(gameObject.transform.position);
     }
 
-    Vector3 SnapToGridPosition(p Vector3)
+    Vector3 SnapToGridPosition(Vector3 p)
     {
-        private Vector3 snapped = new Vector3()
+        int x = (int)Mathf.Round((p.x-offsetX)/delta);
+        int y = (int)Mathf.Round((p.y-offsetY)/delta);
+        x = Math.Max(Math.Min(x,columns),0);
+        y = Math.Max(Math.Min(y,rows),0);
+        p.x = (float) x*delta+offsetX;
+        p.y = (float) y*delta+offsetY;
+        return p;
     }
-
-    int 
 }
