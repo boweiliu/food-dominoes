4dc47515edf206c1b12f92bacffffa550586df06
Bowei Liu <liubowei@gmail.com>
2019-11-10 22:23:17 -0800

    explode the platters maybe

--- Assets/Scripts/PlatterRenderer.cs
+++ Assets/Scripts/PlatterRenderer.cs
@@ -14,11 +14,18 @@ public class PlatterRenderer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(this.transform.GetChild(0).gameObject);
         
     }
 
-    public void setPlatter(Platter platter)
+    public void setPlatter(/* Platter platter */)
     {
+        auto thisPlatterSprite = this.transfrom.GetChild(0).gameObject;
+        thisPlatterSprite.Destroy();
+    }
 
+    private void OnMouseUp()
+    {
+        this.setPlatter();
     }
 }
