3fd84f56f8d5f2cf1aeab77377136304a9a8c236
kodomazer <brianzha@gmail.com>
2019-11-10 21:50:48 -0800

    Project Structure
    Make some shell classes with various functions

---
+++ Assets/Scripts/Table.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Table : MonoBehaviour
+{
+    FoodType[] RequestedFood;
+    bool[] fed;
+
+    public int dishCount
+    {
+        get
+        {
+            return RequestedFood.Length;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void feedDish(FoodType food)
+    {
+
+    }
+
+    public void setFoodRequest(FoodType[] foods)
+    {
+
+    }
+}
