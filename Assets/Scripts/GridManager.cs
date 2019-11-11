aff3ec635053a9f045e27e1848e822be38325459
Alice Zhao <alice.zhao@citcon-inc.com>
2019-11-10 21:07:55 -0800

    not done

---
+++ Assets/Scripts/GridManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    public int rows;
+    public float offsetX;
+    public int columns;
+    public float offsetY;
+    public float tileScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    Vector3 SnapToGridPosition(p Vector3)
+    {
+        private Vector3 snapped = new Vector3()
+    }
+
+    int 
+}
