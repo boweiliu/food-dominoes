34f7b21320fbfe3c7da5f3486c09264ae1654c8c
Bowei Liu <liubowei@gmail.com>
2019-11-10 22:25:29 -0800

    platters successfully explode upon placing

--- Assets/Scripts/PlatterRenderer.cs
+++ Assets/Scripts/PlatterRenderer.cs
@@ -14,14 +14,13 @@ public class PlatterRenderer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(this.transform.GetChild(0).gameObject);
         
     }
 
     public void setPlatter(/* Platter platter */)
     {
-        auto thisPlatterSprite = this.transfrom.GetChild(0).gameObject;
-        thisPlatterSprite.Destroy();
+        GameObject thisPlatterSprite = this.transform.GetChild(0).gameObject;
+        GameObject.Destroy(thisPlatterSprite);
     }
 
     private void OnMouseUp()
